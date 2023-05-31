1be3a70f3e2f86e3b567c9b8001b1ec74ff4f23c
Marvin Kosmider <marvin.kosmider@technikum-wien.at>
2023-05-31 19:30:51 +0200

    initial commit

---
+++ src/main/java/at/technikum/slmbbcalcapi/SlmBbCalcApiApplication.java
@@ -0,0 +1,13 @@
+package at.technikum.slmbbcalcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlmBbCalcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlmBbCalcApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/at/technikum/slmbbcalcapi/SlmBbCalcApiApplicationTests.java
@@ -0,0 +1,13 @@
+package at.technikum.slmbbcalcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlmBbCalcApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
